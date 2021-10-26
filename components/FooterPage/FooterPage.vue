dc366f30a044ec6b1fc6434fd37a522c32e25c18
Quang Vinh <quangvinh2015vi@gmail.com>
2021-10-26 11:54:01 +0700

    them style getResponse

--- components/FooterPage/FooterPage.vue
+++ components/FooterPage/FooterPage.vue
@@ -1,72 +1,73 @@
 <template>
   <!-- Begin Mailchimp Signup Form -->
-  <footer class="bg-primary mt-4 border" style="font-size:14px">
+  <footer class="bg-primary mt-4 border" style="font-size: 14px">
     <link
       href="https://fonts.googleapis.com/icon?family=Material+Icons"
       rel="stylesheet"
     />
-    <!-- Begin Mailchimp Signup Form -->
+
     <div class="container">
       <div class="row p-5 text-light">
         <div class="col-4 mr-2">
-          <h3>Về Chúng Tôi</h3>
-          <p style="font-size:17px">
+          <h4>Về Chúng Tôi</h4>
+          <p style="font-size: 17px">
             webbansachonline.netlify.app là nơi giúp bạn mua sách tiện lợi giá
             cả phải chăng
           </p>
         </div>
-        <div class="col-3 mr-2" style="font-size:17px">
-          <h3>Đường Dẫn</h3>
+        <div class="col-3 mr-2" style="font-size: 17px">
+          <h4>Đường Dẫn</h4>
           <div class="mt-2">
-            <a href="#" class="text-light" style="text-decoration:none"
+            <a href="#" class="text-light" style="text-decoration: none"
               >Trang Chủ</a
             >
           </div>
           <div class="mt-2">
-            <a href="#" class="text-light" style="text-decoration:none"
+            <a href="#" class="text-light" style="text-decoration: none"
               >Về Chúng Tôi</a
             >
           </div>
           <div class="mt-2">
-            <a href="#" class="text-light" style="text-decoration:none"
+            <a href="#" class="text-light" style="text-decoration: none"
               >Thông Tin Liên Lạc</a
             >
           </div>
           <div class="mt-2">
-            <a href="#" class="text-light" style="text-decoration:none"
+            <a href="#" class="text-light" style="text-decoration: none"
               >Dịch vụ</a
             >
           </div>
           <div class="mt-2">
-            <a href="#" class="text-light" style="text-decoration:none"
+            <a href="#" class="text-light" style="text-decoration: none"
               >Điều Kiện Chính Sách</a
             >
           </div>
         </div>
         <div class="col-4">
-          <h3 >Thông Tin Liên Hệ</h3>
+          <h4>Thông Tin Liên Hệ</h4>
 
-          <ul style="list-style-type:none;padding-left:0">
+          <ul style="list-style-type: none; padding-left: 0">
             <li class="row">
-              <span class="col-1"><i class="material-icons ">&#xe0c8;</i></span>
-              <span class="col-10" style="font-size:16px">
+              <span class="col-1"><i class="material-icons">&#xe0c8;</i></span>
+              <span class="col-10" style="font-size: 16px">
                 An Dương Vương<br />Quận 5, Thành phố Hồ Chí Minh<br />Việt Nam
               </span>
             </li>
             <li class="row">
               <span class="col-1"><i class="material-icons">&#xe0cd;</i></span>
-              <span class="col-10" style="font-size:16px">
+              <span class="col-10" style="font-size: 16px">
                 +84 943 634 312<br />+84 987 654 321
               </span>
             </li>
             <li class="row">
               <span class="col-1"><i class="material-icons">&#xe0be;</i></span>
-              <span class="col-10" style="font-size:16px">
+              <span class="col-10" style="font-size: 16px">
                 abcxyz@gmail.com
               </span>
             </li>
           </ul>
-
+          <!-- Begin Mailchimp Signup Form -->
+          
           <div id="mc_embed_signup">
             <form
               action="https://gmail.us5.list-manage.com/subscribe/post?u=84eae2e94b1a31aa0641ad4b8&amp;id=c71e9346f6"
@@ -83,26 +84,26 @@
                     type="email"
                     value=""
                     name="EMAIL"
-                    class="required email"
+                    class="required email form-control"
                     id="mce-EMAIL"
-                    placeholder="Subscribe email"
+                    placeholder="Subscribe email MailChimp"
                   />
                 </div>
                 <div id="mce-responses" class="clear">
                   <div
                     class="response"
                     id="mce-error-response"
-                    style="display:none"
+                    style="display: none"
                   ></div>
                   <div
                     class="response"
                     id="mce-success-response"
-                    style="display:none"
+                    style="display: none"
                   ></div>
                 </div>
                 <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
                 <div
-                  style="position: absolute; left: -5000px;"
+                  style="position: absolute; left: -5000px"
                   aria-hidden="true"
                 >
                   <input
@@ -119,36 +120,55 @@
                     name="subscribe"
                     id="mc-embedded-subscribe"
                     class="button"
-                    style="background:rgb(13, 92, 182)"
+                    style="background: rgb(13, 92, 182)"
                   />
                 </div>
               </div>
             </form>
           </div>
+          <div class="p-2">
+            <!-- GetResponse -->
+            <form
+              action="https://app.getresponse.com/add_subscriber.html"
+              accept-charset="utf-8"
+              method="post"
+            >
+              <!-- Trường email (bắt buộc) -->
+              <div>
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Subscribe email GetResponse"
+                  class="form-control"
+                />
+              </div>
+              
+              <!-- Mã thông báo danh bạ -->
+              <!-- Nhận mã thông báo tại: https://app.getresponse.com/campaign_list.html https://app.getresponse.com/campaign_list.html -->
+              <input type="hidden" name="campaign_token" value="rTyk5" />
+              <!-- Thêm người đăng ký vào chuỗi theo dõi với ngày được xác định (tùy chọn) -->
+              <input type="hidden" name="start_day" value="0" />
+              <!-- Nút Người đăng ký -->
+              <input
+                type="submit"
+                value="Subscribe"
+                class="btn btn-warning mt-3"
+                style="font:14px Helvetica, Arial, sans-serif;width:110px"
+              />
+            </form>
+            <!-- End GetResponse -->
+          </div>
+
+          <!--End mc_embed_signup-->
         </div>
       </div>
     </div>
-    <!--End mc_embed_signup-->
-
-    <!-- GetResponse -->
-<form action="https://app.getresponse.com/add_subscriber.html" accept-charset="utf-8" method="post">
-	<!-- Trường email (bắt buộc) -->
-	email: <input type="text" name="email"/><br/>
-	<!-- Mã thông báo danh bạ -->
-	<!-- Nhận mã thông báo tại: https://app.getresponse.com/campaign_list.html https://app.getresponse.com/campaign_list.html -->
-	<input type="hidden" name="campaign_token" value="rTyk5" />
-	<!-- Thêm người đăng ký vào chuỗi theo dõi với ngày được xác định (tùy chọn) -->
-	<input type="hidden" name="start_day" value="0" />
-	<!-- Nút Người đăng ký -->
-	<input type="submit" value="Subscribe"/>
-</form>
-    <!-- End GetResponse -->
   </footer>
   <!--End mc_embed_signup-->
 </template>
 
 <script>
-export default {};
+export default {}
 </script>
 
 <style type="text/css">
@@ -161,4 +181,4 @@ We recommend moving this block and the preceding CSS link to the HEAD of your HT
   clear: left;
   font: 14px Helvetica, Arial, sans-serif;
 }
- </style>
+</style>
